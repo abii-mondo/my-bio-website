a8b44ef7c1a78ea4a34a2b396a9a8b5b9312e56e
Abraham Mondo <abrahammondo@switchmaven.com>
2020-10-20 16:22:33 +1000

    my bio app - first commit

--- index.js
+++ index.js
@@ -2,11 +2,16 @@
 const express = require('express')
 
 const path = require('path')
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = 3000
 
+const urlencodedParser = bodyParser.urlencoded({extended: true})
+
 app.use(express.static('public'))
+
+
 
 
 app.get('/', function(req,res){
@@ -17,16 +22,27 @@ app.get('/contact', function(req,res){
 	res.sendFile(path.resolve(__dirname,'pages/contact.html'))
 })
 
-app.get('/post', function(req,res){
-
-	res.sendFile(path.resolve(__dirname,'pages/post.html'))
+app.post('/contact', urlencodedParser, function(req, res){
+	console.log(req.body)
+	let name = `<h3>Hey ${req.body.name}, Thanks for contacting me. I will get back to you! </h3>`
+	if (!req.body.email || !req.body.message) return res.sendFile(path.resolve(__dirname, 'pages/contact-unsuccess.html'))
+	res.sendFile(path.resolve(__dirname, 'pages/contact-success.html'), name)
 })
 
-app.get('/about', function(req,res){
-	res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+app.get('/education', function(req,res){
+
+	res.sendFile(path.resolve(__dirname,'pages/education.html'))
+})
+
+app.get('/work', function(req,res){
+	res.sendFile(path.resolve(__dirname, 'pages/work.html'))
+})
+
+app.get('/skills', function(req,res){
+	res.sendFile(path.resolve(__dirname, 'pages/skills.html'))
 })
 
 app.listen(port, (req, res) => {
-	console.log(`Blog App listening on http://localhost:${port}`)
+	console.log(`Bio Profile App listening on http://localhost:${port}`)
 })
 
